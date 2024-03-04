24ef83ee3567658b074fca3939c63212bae9c17a
PapelConPera <lionel.copajira@gmail.com>
2024-03-04 11:53:08 -0300

    ultimos pasos

--- 2do.cuatri.sql
+++ 2do.cuatri.sql
@@ -10,3 +10,17 @@ SELECT actor_id
 FROM actor
 WHERE first_name like 'Michael' AND last_name = 'Jordan'
 
+--Mostrar toda la lista de todos los actores de michael jordan
+SELECT a.first_name,a.last_name FROM actor a 
+JOIN film f ON a.actor_id
+WHERE first_name like 'Michael' AND last_name like 'jordan'
+;
+
+--crear los id de las peliculas y actor que creee
+insert into actor (actor_id,film_id) values ('201','1001');
+
+--mostrar el unico nombre en la lista con distintas peliculas 
+SELECT a.actor_id, a.first_name, a.last_name, f.title AS film_title
+FROM actor a JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+WHERE a.actor_id = 1 LIMIT 3; 
